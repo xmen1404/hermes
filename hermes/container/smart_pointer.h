56618c80bf5f4d603b244e1372b8f8adedaf0d9b
vspm <xmen1404@gmail.com>
2025-02-06 11:40:45 +0700

    feat: container::WeakPtr implementation & unit tests

--- hermes/container/smart_pointer.h
+++ hermes/container/smart_pointer.h
@@ -1,33 +1,269 @@
 #pragma once
 
-#include <glog/logging.h>
+#include <atomic>
+#include <memory>
+#include <type_traits>
 
 namespace hermes::container {
 
+/**
+ * @brief A simplified unique pointer that exclusively owns a dynamically
+ * allocated object.
+ *
+ * @tparam T The type of the managed object.
+ */
 template <typename T> class UniquePtr {
 public:
+  /**
+   * @brief Constructs a UniquePtr that takes ownership of the given pointer.
+   *
+   * @param ptr A pointer to a dynamically allocated object of type T (defaults
+   * to nullptr).
+   */
   explicit UniquePtr(T *ptr = nullptr) { data_ = ptr; }
 
+  /**
+   * @brief Move constructor that transfers ownership from another UniquePtr.
+   *
+   * @param rhs An rvalue reference to another UniquePtr.
+   */
   UniquePtr(UniquePtr &&rhs) : data_{rhs.data_} { rhs.data_ = nullptr; }
 
+  /**
+   * @brief Destructor that deletes the managed object.
+   */
   ~UniquePtr() noexcept { delete data_; }
 
+  // Deleted copy assignment operators to enforce unique ownership.
   UniquePtr &operator=(UniquePtr &rhs) = delete;
   UniquePtr &operator=(const UniquePtr &rhs) = delete;
-  UniquePtr &operator=(UniquePtr &&rhs) { std::swap(data_, rhs.data_); }
+
+  /**
+   * @brief Move assignment operator that transfers ownership.
+   *
+   * @param rhs An rvalue reference to another UniquePtr.
+   * @return A reference to this UniquePtr.
+   */
+  UniquePtr &operator=(UniquePtr &&rhs) {
+    std::swap(data_, rhs.data_);
+    return *this;
+  }
 
 public:
+  /**
+   * @brief Returns the raw pointer to the managed object.
+   *
+   * @return A pointer to the managed object.
+   */
   T *Get() const noexcept { return data_; }
 
+  /**
+   * @brief Explicit conversion to bool to check if the UniquePtr owns an
+   * object.
+   *
+   * @return true if the managed pointer is not nullptr, false otherwise.
+   */
   explicit operator bool() const noexcept { return data_ != nullptr; }
 
 private:
   T *data_;
 };
 
+/**
+ * @brief Creates a UniquePtr managing a new instance of T.
+ *
+ * Constructs an object of type T using the provided arguments and returns a
+ * UniquePtr that owns it.
+ *
+ * @tparam T The type of the object to be created.
+ * @tparam Args The types of the arguments used to construct the object.
+ * @param params Parameters forwarded to T's constructor.
+ * @return A UniquePtr that manages the newly created object.
+ */
 template <typename T, typename... Args>
 inline UniquePtr<T> MakeUnique(Args &&...params) {
   return UniquePtr<T>{new T{std::forward<decltype(params)>(params)...}};
 }
+
+/**
+ * @brief Control block for SharedPtr, managing reference counts.
+ */
+struct SpControlBlock {
+  std::atomic<size_t> ref_cnt;      ///< Shared pointer count.
+  std::atomic<size_t> weak_ref_cnt; ///< Weak pointer count.
+
+  SpControlBlock() = delete;
+  SpControlBlock(bool is_weak) : ref_cnt(!is_weak), weak_ref_cnt(is_weak) {}
+};
+
+template <typename T> class WeakPtr;
+
+/**
+ * @brief A simple shared pointer implementation.
+ * @tparam T The type of the managed object.
+ */
+template <typename T> class SharedPtr {
+public:
+  /// Default constructor: creates an empty SharedPtr.
+  SharedPtr() {}
+
+  /// Constructs a SharedPtr that owns the given pointer.
+  SharedPtr(T *ptr) : data_ptr_(ptr), ctrl_ptr_(new SpControlBlock(false)) {}
+
+  SharedPtr(T *data_ptr, SpControlBlock *ctrl_ptr)
+      : data_ptr_(data_ptr), ctrl_ptr_(ctrl_ptr) {}
+
+  /// Copy constructor: increments the reference count.
+  SharedPtr(const SharedPtr &rhs) {
+    if (rhs.ctrl_ptr_ != nullptr) {
+      data_ptr_ = rhs.data_ptr_;
+      ctrl_ptr_ = rhs.ctrl_ptr_;
+      ctrl_ptr_->ref_cnt.fetch_add(1, std::memory_order_relaxed);
+    }
+  }
+
+  /// Move constructor: increments the reference count.
+  SharedPtr(SharedPtr &&rhs) {
+    if (rhs.ctrl_ptr_ != nullptr) {
+      data_ptr_ = rhs.data_ptr_;
+      ctrl_ptr_ = rhs.ctrl_ptr_;
+      ctrl_ptr_->ref_cnt.fetch_add(1, std::memory_order_relaxed);
+    }
+  }
+
+  /// Destructor: releases ownership.
+  ~SharedPtr() { Reset(); }
+
+public:
+  /// Releases ownership. Deletes the managed object if this was the last owner.
+  void Reset() {
+    if (!ctrl_ptr_)
+      return;
+    auto prev_cnt = ctrl_ptr_->ref_cnt.fetch_add(-1, std::memory_order_acq_rel);
+    if (prev_cnt == 1) {
+      delete data_ptr_;
+      if (!ctrl_ptr_->weak_ref_cnt.load(std::memory_order_acquire))
+        delete ctrl_ptr_;
+    }
+    data_ptr_ = nullptr;
+    ctrl_ptr_ = nullptr;
+  }
+
+  /// Resets to manage a new pointer.
+  void Reset(T *ptr) {
+    Reset();
+    ctrl_ptr_ = new SpControlBlock(false);
+    data_ptr_ = ptr;
+  }
+
+  /// Returns the managed pointer.
+  T *Get() noexcept { return data_ptr_; }
+
+  /// Returns true if this is the unique owner.
+  bool IsUnique() const noexcept {
+    return !ctrl_ptr_ ||
+           ctrl_ptr_->ref_cnt.load(std::memory_order_relaxed) == 1;
+  }
+
+private:
+  SpControlBlock *ctrl_ptr_{nullptr}; ///< Pointer to the control block.
+  T *data_ptr_{nullptr};              ///< Pointer to the managed object.
+
+private:
+  friend class WeakPtr<T>;
+};
+
+template <typename T, typename... Args>
+inline static SharedPtr<T> MakeShared(Args &&...params) {
+  return SharedPtr<T>(new T{std::forward<Args>(params)...},
+                      new SpControlBlock{false});
+}
+
+/**
+ * @brief A non-owning weak pointer.
+ *
+ * @tparam T Type of the object.
+ */
+template <typename T> class WeakPtr {
+public:
+  /// Default constructor.
+  constexpr WeakPtr() noexcept {}
+
+  /// Copy constructor: increments weak count if control block exists.
+  WeakPtr(const WeakPtr &rhs) noexcept {
+    if (rhs.ctrl_ptr_ != nullptr) {
+      ctrl_ptr_ = rhs.ctrl_ptr_;
+      data_ptr_ = rhs.data_ptr_;
+      ctrl_ptr_->weak_ref_cnt.fetch_add(1, std::memory_order_relaxed);
+    }
+  }
+
+  /// Construct from SharedPtr: increments weak count if control block exists.
+  WeakPtr(const SharedPtr<T> &rhs) noexcept {
+    if (rhs.ctrl_ptr_ != nullptr) {
+      ctrl_ptr_ = rhs.ctrl_ptr_;
+      data_ptr_ = rhs.data_ptr_;
+      ctrl_ptr_->weak_ref_cnt.fetch_add(1, std::memory_order_relaxed);
+    }
+  }
+
+  /**
+   * @brief Assign from a SharedPtr.
+   *
+   * Releases current control block and links to the new one.
+   */
+  WeakPtr &operator=(const SharedPtr<T> &rhs) noexcept {
+    Reset();
+    if (rhs.ctrl_ptr_ != nullptr) {
+      ctrl_ptr_ = rhs.ctrl_ptr_;
+      data_ptr_ = rhs.data_ptr_;
+      ctrl_ptr_->weak_ref_cnt.fetch_add(1, std::memory_order_relaxed);
+    }
+    return *this;
+  }
+
+  /// @brief Checks if the managed object has expired.
+  bool IsExpired() const noexcept {
+    return ctrl_ptr_ == nullptr ||
+           !ctrl_ptr_->ref_cnt.load(std::memory_order_relaxed);
+  }
+
+  /**
+   * @brief Locks the weak pointer to return a SharedPtr.
+   *
+   * Increments the strong count if object exists; else returns empty SharedPtr.
+   */
+  SharedPtr<T> Lock() {
+    if (ctrl_ptr_ == nullptr)
+      return {};
+
+    auto prev_cnt = ctrl_ptr_->ref_cnt.fetch_add(1, std::memory_order_acquire);
+    if (prev_cnt == 0) {
+      ctrl_ptr_->ref_cnt.fetch_add(-1, std::memory_order_acq_rel);
+      return {};
+    }
+    return SharedPtr<T>{data_ptr_, ctrl_ptr_};
+  }
+
+  /**
+   * @brief Releases the weak pointer.
+   *
+   * Decrements weak count and deletes the control block if needed.
+   */
+  void Reset() {
+    if (ctrl_ptr_ == nullptr)
+      return;
+    auto prev_cnt =
+        ctrl_ptr_->weak_ref_cnt.fetch_add(-1, std::memory_order_acq_rel);
+    if (prev_cnt == 1 && !ctrl_ptr_->ref_cnt.load(std::memory_order_acquire))
+      delete ctrl_ptr_;
+    ctrl_ptr_ = nullptr;
+    data_ptr_ = nullptr;
+  }
+
+private:
+  SpControlBlock *ctrl_ptr_{nullptr}; ///< Pointer to the control block.
+  T *data_ptr_{nullptr};              ///< Pointer to the managed object.
+};
 
 } // namespace hermes::container
--- test/unit_test/container/smart_pointer_test.cpp
+++ test/unit_test/container/smart_pointer_test.cpp
@@ -4,18 +4,243 @@
 
 using namespace hermes::container;
 
-TEST_CASE("Init Test") {
+TEST_CASE("Init Test", "[UniquePtr]") {
   auto uptr = UniquePtr<int>(new int(12));
   REQUIRE(*uptr.Get() == 12);
 }
 
-TEST_CASE("MakeUnique Test") {
+TEST_CASE("MakeUnique Test", "[UniquePtr]") {
   auto uptr = MakeUnique<int>(12);
   REQUIRE(*uptr.Get() == 12);
 }
 
-TEST_CASE("Transfer Ownership Test") {
+TEST_CASE("Transfer Ownership Test", "[UniquePtr]") {
   auto uptr = UniquePtr<int>(new int(12));
   auto n_ptr = std::move(uptr);
   REQUIRE(*n_ptr.Get() == 12);
+}
+
+TEST_CASE("Init Test", "[SharedPtr]") {
+  auto *data = new int{12};
+  auto shared_ptr = SharedPtr<int>{data};
+  REQUIRE(*shared_ptr.Get() == 12);
+}
+
+TEST_CASE("RefCnt Test", "[SharedPtr]") {
+  int del_cnt = 0;
+
+  class TestClass {
+    int *del_cnt_;
+
+  public:
+    TestClass(int *ptr) : del_cnt_{ptr} {}
+    ~TestClass() { (*del_cnt_)++; }
+  };
+
+  {
+    auto obj_ptr = new TestClass(&del_cnt);
+    auto ptr1 = SharedPtr<TestClass>(obj_ptr);
+    // Initially unique.
+    REQUIRE(ptr1.IsUnique());
+
+    // Copy increases ref count.
+    auto ptr2 = ptr1;
+    REQUIRE(!ptr1.IsUnique());
+
+    SharedPtr<TestClass> ptr3{ptr2};
+    {
+      SharedPtr<TestClass> ptr4{ptr1};
+      SharedPtr<TestClass> ptr5{ptr3};
+      // Test move construction.
+      SharedPtr<TestClass> ptr6{std::move(ptr2)};
+      REQUIRE(ptr6.Get() == ptr5.Get());
+    }
+  }
+
+  // After all SharedPtrs are destroyed, the object should be deleted exactly
+  // once.
+  REQUIRE(del_cnt == 1);
+}
+
+TEST_CASE("Reset Test", "[SharedPtr]") {
+  int del_cnt = 0;
+
+  class TestClass {
+    int *del_cnt_;
+
+  public:
+    TestClass(int *ptr) : del_cnt_{ptr} {}
+    ~TestClass() { (*del_cnt_)++; }
+  };
+
+  {
+    // Create a SharedPtr and then reset it to release ownership.
+    SharedPtr<TestClass> ptr(new TestClass(&del_cnt));
+    REQUIRE(ptr.Get() != nullptr);
+    ptr.Reset();
+    // After Reset, the pointer should be null.
+    REQUIRE(ptr.Get() == nullptr);
+  }
+  // The managed object should have been deleted.
+  REQUIRE(del_cnt == 1);
+}
+
+TEST_CASE("Reset with new pointer Test", "[SharedPtr]") {
+  int del_cnt = 0;
+
+  class TestClass {
+    int *del_cnt_;
+
+  public:
+    TestClass(int *ptr) : del_cnt_{ptr} {}
+    ~TestClass() { (*del_cnt_)++; }
+  };
+
+  {
+    // Create a SharedPtr managing one object.
+    SharedPtr<TestClass> ptr(new TestClass(&del_cnt));
+    // Now reset with a new pointer.
+    ptr.Reset(new TestClass(&del_cnt));
+    REQUIRE(ptr.Get() != nullptr);
+    // The old object should have been deleted already.
+    REQUIRE(del_cnt == 1);
+  }
+  // When ptr goes out of scope, the new object is deleted.
+  // Total deletion count should now be 2.
+  REQUIRE(del_cnt == 2);
+}
+
+TEST_CASE("Copy and Move Test", "[SharedPtr]") {
+  int del_cnt = 0;
+
+  class TestClass {
+    int *del_cnt_;
+
+  public:
+    TestClass(int *ptr) : del_cnt_{ptr} {}
+    ~TestClass() { (*del_cnt_)++; }
+  };
+
+  SharedPtr<TestClass> ptr1(new TestClass(&del_cnt));
+  {
+    // Test copy constructor.
+    SharedPtr<TestClass> ptr2(ptr1);
+    // Test move constructor.
+    SharedPtr<TestClass> ptr3(std::move(ptr2));
+    // Both ptr1 and ptr3 should manage the same object.
+    REQUIRE(ptr1.Get() == ptr3.Get());
+    // With at least two SharedPtr instances, the pointer is not unique.
+    REQUIRE(!ptr1.IsUnique());
+  }
+  // After inner block, only ptr1 remains so it becomes unique.
+  REQUIRE(ptr1.IsUnique());
+  // When ptr1 goes out of scope, the object is deleted.
+  // (Deletion count is verified in the next test block.)
+}
+
+TEST_CASE("Self-contained Lifetime Test", "[SharedPtr]") {
+  int del_cnt = 0;
+
+  class TestClass {
+    int *del_cnt_;
+
+  public:
+    TestClass(int *ptr) : del_cnt_{ptr} {}
+    ~TestClass() { (*del_cnt_)++; }
+  };
+
+  {
+    SharedPtr<TestClass> ptr(new TestClass(&del_cnt));
+    {
+      auto ptr_copy = ptr;
+      REQUIRE(ptr_copy.Get() == ptr.Get());
+      REQUIRE(!ptr_copy.IsUnique());
+    }
+    // After inner block, only the original pointer remains.
+    REQUIRE(ptr.IsUnique());
+  }
+  // When ptr goes out of scope, the object is deleted exactly once.
+  REQUIRE(del_cnt == 1);
+}
+
+struct Dummy {
+  int value;
+  Dummy(int v) : value(v) {}
+};
+
+TEST_CASE("MakeShared creates and initializes object",
+          "[SharedPtr][MakeShared]") {
+  auto sp = MakeShared<Dummy>(42);
+
+  // Check that the managed object is not null.
+  REQUIRE(sp.Get() != nullptr);
+
+  // Check that the object is correctly initialized.
+  REQUIRE(sp.Get()->value == 42);
+
+  // When first created, the shared pointer should be unique.
+  REQUIRE(sp.IsUnique());
+}
+
+TEST_CASE("MakeShared deletes object when last SharedPtr goes out of scope",
+          "[SharedPtr][MakeShared]") {
+  int deletionCounter = 0;
+
+  struct TestObject {
+    int *deletionCounter;
+    TestObject(int *dc) : deletionCounter(dc) {}
+    ~TestObject() { (*deletionCounter)++; }
+  };
+
+  {
+    auto sp = MakeShared<TestObject>(&deletionCounter);
+    REQUIRE(sp.Get() != nullptr);
+    REQUIRE(sp.IsUnique());
+
+    {
+      // Create a copy to increase the reference count.
+      auto sp2 = sp;
+      REQUIRE(!sp.IsUnique());
+    }
+    // After sp2 goes out of scope, sp should be unique again.
+    REQUIRE(sp.IsUnique());
+  }
+  // After sp goes out of scope, the object should be deleted.
+  REQUIRE(deletionCounter == 1);
+}
+
+TEST_CASE("Init Test", "[WeakPtr]") {
+  auto *data = new int{12};
+  auto shared_ptr = SharedPtr<int>{data};
+  REQUIRE(shared_ptr.IsUnique());
+
+  auto weak_ptr = WeakPtr<int>{shared_ptr};
+  REQUIRE(weak_ptr.IsExpired() == false);
+}
+
+TEST_CASE("RefCnt Test", "[WeakPtr]") {
+  int del_cnt = 0;
+
+  class TestClass {
+    int *del_cnt_;
+
+  public:
+    TestClass(int *ptr) : del_cnt_{ptr} {}
+    ~TestClass() { (*del_cnt_)++; }
+  };
+
+  {
+    auto obj_ptr = new TestClass(&del_cnt);
+    auto ptr1 = SharedPtr<TestClass>(obj_ptr);
+    // Initially unique.
+    REQUIRE(ptr1.IsUnique());
+
+    auto weak_ptr = WeakPtr<TestClass>{ptr1};
+    auto ptr2 = weak_ptr.Lock();
+    REQUIRE(!ptr2.IsUnique());
+  }
+
+  // After all SharedPtrs are destroyed, the object should be deleted exactly
+  // once.
+  REQUIRE(del_cnt == 1);
 }
